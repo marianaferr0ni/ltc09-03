02e1cdae995fe7a9e8274cc56820610d2c9888f3
marianaferr0ni <>
2022-03-09 18:44:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+class Retangulo
+{
+  constructor(altura, largura)
+  {
+      this.altura = altura;
+      this.largura = largura;
+  }
+  mostrarAtributos()
+  {
+    return "Altura: " + this.altura + " Largura: " + this.largura;
+  }
+  calcularArea()
+  {
+    return "Área do retângulo: " + this.largura * this.altura;
+  }
+  area()
+  {
+    return this.calcularArea();
+  }
+}
+
+let quadrado = new Retangulo("10", "10");
+let retangulo = new Retangulo("60", "30");
+
+console.log(quadrado.area())
+console.log(retangulo.area())
+//console.log(quadrado.mostrarAtributos())
+//console.log(quadrado.calcularArea())
+//console.log(retangulo.mostrarAtributos())
+//console.log(retangulo.calcularArea())
